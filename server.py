2db48141b866c6f5ee38a242a049ef0b953a92f2
mvanegas10 <m.vanegas10@uniandes.edu.co>
2016-10-29 12:02:23 -0500

    Changed host and port

--- server.py
+++ server.py
@@ -136,4 +136,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-	app.run(host= '127.0.0.1')
+	app.run(host= '0.0.0.0',port=9345)
